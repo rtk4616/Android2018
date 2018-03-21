77b6418f088d65e2d367915ebebae6b845cf154c
nlhsueh <nlhsueh@fcu.edu.tw>
2018-03-21 08:47:36 +0800

    Customerize the one row text in the LIST VIEW

--- ListDemo/app/src/main/java/e/nlhsueh/listdemo/MainActivity.java
+++ ListDemo/app/src/main/java/e/nlhsueh/listdemo/MainActivity.java
@@ -11,8 +11,8 @@ import android.widget.Toast;
 public class MainActivity extends ListActivity {
 
 
-    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
+    String[] FCU_snack = {"甘丼 養生烏梅湯","燒鳥串燒","燒鳥串炸","朴大哥韓式炸雞",
+            "多士號","黃G紅炸雞","古紅瓦早味米血","造堡", "木巷咖啡"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +20,16 @@ public class MainActivity extends ListActivity {
         setContentView(R.layout.activity_main);
 
         ArrayAdapter adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,
-//                R.layout.list_item,
-//                R.id.textView,
-                mobileArray);
+//                android.R.layout.simple_list_item_1,
+                R.layout.list_item,
+                R.id.textView,
+                FCU_snack);
 
         setListAdapter(adapter);
     }
 
     protected void onListItemClick (ListView l, View v, int position, long id) {
-        Toast.makeText(this, Integer.toString(position), Toast.LENGTH_SHORT).show();
-
+        String msg = "就是要吃" + FCU_snack[position];
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
